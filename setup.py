8b283694050447c071b00912e590b1dd30082a41
Emanuele Rocca <ema@linux.it>
2012-08-22 09:48:32 +0200

    Remove docs.py: the long description can be generated via setup.py --long-description

--- setup.py
+++ setup.py
@@ -2,8 +2,32 @@
 
 from setuptools import setup, find_packages
 
-from docs import longdesc
 import codicefiscale
+
+longdesc = codicefiscale.__doc__
+
+longdesc = """%s
+
+codicefiscale Module Documentation
+==================================
+
+A quick example
+---------------
+>>> import datetime
+>>> from codicefiscale import build
+>>>
+>>> build('Rocca', 'Emanuele', datetime.datetime(1983, 11, 18), 'M', 'D969')
+'RCCMNL83S18D969H'
+
+Module Contents
+---------------
+""" % longdesc
+
+for what in dir(codicefiscale):
+    if not what.startswith("__"):
+        obj = getattr(codicefiscale, what)
+        if callable(obj) and obj.__doc__:
+            longdesc += obj.__doc__ + "\n\n\n"
 
 setup(
        name = 'codicefiscale',
